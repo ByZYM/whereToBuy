13db098aa7cc59f2c2d3751e51a32d9c77694ee2
hotdogwhat <35364846+hotdogwhat@users.noreply.github.com>
2019-01-10 17:55:37 +0800

    实现主页商品分类显示
    实现主页商品分类显示

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mainPage">
+  <div class="mainPage" id="vfor">
     <div class="container">
       <div class="top">
         <a href="#" style="float: left;">定位</a>
@@ -51,42 +51,17 @@
         </div>
       </div>
 
-      <div class="container">
+      <div class="container" >
         <div class="row">
           <span>分类商品</span>
         </div>
         <div class="box">
           <div class="row">
-            <div class="col-xs-4 col-md-4">
+            <div v-if="index<6" v-for="(cat , index) in catList" class="col-xs-4 col-md-4">
               <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
+                <img :src="require('../assets/catIcon/'+index+'.jpg')"/>
+                <!--<img src="../assets/catIcon/0.jpg"/>-->
+                {{cat.name}}
               </a>
             </div>
           </div>
@@ -99,20 +74,17 @@
         </div>
         <div class="box">
           <div class="row">
-            <div class="col-xs-4 col-md-4">
+            <div v-for="goods in newGoodsList" class="col-xs-4 col-md-4">
               <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
+                <!--<img src="{{goods.images.split(';')[0]}}" alt="logo">-->
+                <!--<img :src="require(''+goods.images.split(';')[0]+'')"/>-->
+                <!--<img :src="require('../assets/catIcon/'+index+'.jpg')"/>-->
+
+                <img src="../assets/catIcon/3.jpg"/>
+                <!--<img :src="[require(''+goods.images.split(';')[0]+'')]"/>-->
               </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
-            </div>
-            <div class="col-xs-4 col-md-4">
-              <a href="#" class="thumbnail">
-                <img src="../assets/logo.png" alt="logo">
-              </a>
+              {{goods.name}}
+              <!--{{goods.images.split(';')[0]}}-->
             </div>
           </div>
         </div>
@@ -124,12 +96,28 @@
 
 <script>
 export default {
-  name: 'MainPage',
+  el: '#vfor',
+//  name: 'MainPage',
   data () {
     return {
-      msg: 'Welcome to MainPage'
+      catList:[],
+      newGoodsList:[]
     }
+//    return {
+//      msg: 'Welcome to MainPage'
+//    }
+  },
+  mounted(){
+    this.$http.get('http://39.108.70.119:8080/type/findAll').then((res) => {
+      this.catList=res.body;
+    });
+    this.$http.get('http://39.108.70.119:8080/goods/newGoods').then((res) => {
+      this.newGoodsList=res.body;
+//      alert(this.newGoodsList[0].images);
+    });
+
   }
+
 }
 </script>
 
@@ -167,6 +155,9 @@ export default {
   /*border-color: #c70c15;*/
   border:1px solid red;
 }
+/*.row>div{*/
+  /*padding: 0px 0px;*/
+/*}*/
 /*.recommend{*/
   /*zoom:1;*/
   /*overflow: hidden;*/
